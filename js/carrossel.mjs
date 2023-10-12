ffb9e6620da2dc674b579e2ea6074538bc5a9635
Pedro Mattiazzo Goudet <109755812+pmgoudet@users.noreply.github.com>
2023-10-12 19:31:27 +0200

    Carrossel Teste aprovado.

--- js/carrossel.mjs
+++
@@ -1,8 +0,0 @@
-import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs';
-
-const swiper = new Swiper('.swiper', {
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-    },
-  });
