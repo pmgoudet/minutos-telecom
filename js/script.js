66132dba0b922ac3cad0b5aebfe1991a1e7b5094
Pedro Mattiazzo Goudet <109755812+pmgoudet@users.noreply.github.com>
2024-08-30 14:08:39 +0200

    Implementação do site 2.0
    Apenas o header.
    Para desk, tablet e mobile

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-// MENU HAMBURGUER
+// Menu Hamburguer
 
 const mobileBtn = document.querySelector('#btn-mobile')
-const nav = document.querySelector('#nav')
+const nav = document.querySelector('#header__nav-bar')
 
 mobileBtn.addEventListener('click', toggleMenu)
 
 function toggleMenu() {
     nav.classList.toggle('active')
 }
+
