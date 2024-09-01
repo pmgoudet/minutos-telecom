c225dbbab5e976a8e4341a1912b9953af97fa043
Pedro Mattiazzo Goudet <109755812+pmgoudet@users.noreply.github.com>
2024-09-01 15:21:49 +0200

    Slider finalizado
    Acho que tá finalizado, vai saber
    Desk tablet e mobile

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ function toggleMenu() {
     nav.classList.toggle('active')
 }
 
+
+// Swiper
+
+const radios = document.querySelectorAll('input[name="slider"]');
+let currentSlide = 0; 
+const slideInterval = 3000;
+
+function showNextSlide() {
+   radios[currentSlide].checked = false;
+   currentSlide = (currentSlide + 1) % radios.length;
+   radios[currentSlide].checked = true;
+}
+
+setInterval(showNextSlide, slideInterval);
+
+
+
